82a621afb0139f2b6f698dfdeee9a84263d943eb
Стёпкин Егор Сергеевич <29415403@s.mfua.ru>
2023-10-13 11:55:00 +0300

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RoomLibraryStyopkin03PRd4310")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RoomLibraryStyopkin03PRd4310")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f5982aaf-6a3a-4181-9647-afb89395eace")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Room.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibraryStyopkin03PRd4310
+{
+    public class Room
+    {
+        double roomLength;
+        double roomWidth;
+
+        public double RoomLength
+        {
+            get { return roomLength; }
+            get { roomLength = value; }
+        }
+
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            get { roomWidth = value; }
+        }
+
+        /// <summary>
+        /// метод вычисляет периметр комнаты
+        /// </summary>
+        /// <returns>значение периметра</returns>
+        public double RoomPerimetr()
+        {
+            return 2 * (roomLength + roomWidth);
+        }
+        /// <summary>
+        /// метод вычисляет площадь комнаты
+        /// </summary>
+        /// <returns>значение площади</returns>
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+    }
+
+}
