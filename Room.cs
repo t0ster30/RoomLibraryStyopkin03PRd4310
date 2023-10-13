6267026a2cd2ee357322c09529aabf64a9a978e5
t0ster30 <29415403@s.mfua.ru>
2023-10-13 15:57:21 +0300

    polymorphism.

--- Room.cs
+++ Room.cs
@@ -48,6 +48,53 @@ namespace RoomLibraryStyopkin03PRd4310
         {
             return RoomArea() / np;
         }
+        public string Info()
+        {
+            return "Комната площадью " + RoomArea() + "кв.м";
+        }
     }
 
+    public class LivingRoom : Room
+    {
+        int numWin;
+        public int NumWin
+        {
+            get { return numWin; }
+            set { numWin = value; }
+        }
+        /// <summary>
+        /// метод возвращает информацию о комнате
+        /// </summary>
+        /// <returns>строка с информацией о комнате</returns>
+        public string Info()
+        {
+            return "Жилая комната площадью " + RoomArea() + " кв.м, с " + numWin + " окнами";
+        }
+    }
+    public class Office : Room
+    {
+        int numSockets;
+        public int NumSockets
+        {
+            get { return numSockets; }
+            set { numSockets = value; }
+        }
+        /// <summary>
+        /// возвращает максимальное число рабочих мест
+        /// </summary>
+        /// <returns>число мест</returns>
+        public int NumWorkplaces()
+        {
+            int num = Convert.ToInt32(Math.Truncate(RoomArea() / 4.5));
+            return Math.Min(numSockets, num);
+        }
+        /// <summary>
+        /// метод возвращает информацию об офисе
+        /// </summary>
+        /// <returns>строка с информацией</returns>
+        public string Info()
+        {
+            return "Офис на " + NumWorkplaces() + " рабочих мест";
+        }
+    }
 }
